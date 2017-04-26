cddc1f3ce0a3c5d8e5563479ff5c4e7a8891b36a
Francisco Echeverria <fcontreras18@gmail.com>
2017-04-26 14:59:10 -0700

    Add wiki request timeout

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=22376e0ea9aa4b09a77af1cc20cfaf9f";
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=22376e0ea9aa4b09a77af1cc20cfaf9f';
     $.getJSON( nytimesUrl, function(data){
 
       $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -37,6 +37,11 @@ function loadData() {
         $nytHeaderElem.text('NYT Articles could not be loaded');
     });
     
+    // Timeout
+    var wikiRequestTimeout = setTimeout(function(){
+      $wikiElem.text('failed to get wikipedia resources');
+    }, 8000);
+
     // Wikipedia AJAX request
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 
@@ -51,6 +56,8 @@ function loadData() {
           var url = 'http://en.wikipedia.org/wiki/' + articleStr;
           $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
         };
+        
+        clearTimeout(wikiRequestTimeout);
       }
     });
 

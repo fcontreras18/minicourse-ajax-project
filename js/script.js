9d48573268f6144a3a85d020bbf9098bfdd23db8
Francisco Echeverria <fcontreras18@gmail.com>
2017-04-26 15:18:55 -0700

    Add nyt api key to config.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,9 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=22376e0ea9aa4b09a77af1cc20cfaf9f';
+    var nyt_key = config.NYT_KEY
+
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=' + nyt_key;
     $.getJSON( nytimesUrl, function(data){
 
       $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -56,7 +58,7 @@ function loadData() {
           var url = 'http://en.wikipedia.org/wiki/' + articleStr;
           $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
         };
-        
+
         clearTimeout(wikiRequestTimeout);
       }
     });
